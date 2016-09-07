5b7e86793d5de4b97fe0446218b153d8c9050368
Alexander D'Este <alexanderdeste@gmail.com>
2016-09-07 16:46:26 +0200

    feat(): Fixes tests, fixes logics

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,18 +6,18 @@ describe('all', () => {
     expect(dinosaurs.all).to.satisfy(array => array.every(dinosaur => typeof dinosaur === 'string'))
   );
 
-  it('should contain Antrodemus', () => {
+  it('Should contain Antrodemus', () => {
     expect(dinosaurs.all).to.include('Antrodemus');
   });
 });
 
 describe('random', () => {
-  it('should return a random dinosaur', () => {
+  it('Should return a random dinosaur', () => {
     const dino = dinosaurs.random();
     expect(dinosaurs.all).to.include(dino);
   })
 
-  it('should return a an array of random items, if passed a number', () => {
+  it('Should return a an array of random items, if passed a number', () => {
     const dinos = dinosaurs.random(3);
     expect(dinos).to.have.length(3);
     dinos.map(dino => {
